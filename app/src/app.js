c4d0cda7c3f05980c1818d5fed81d85660a2626f
alu0101166247 <alu0101166247@ull.edu.es>
2021-12-05 19:49:59 +0000

    fix

--- app/src/app.js
+++ app/src/app.js
@@ -6,7 +6,7 @@ import history from "./_helpers/history";
 import { PrivateRoute } from "./components";
 import Navbar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { Home } from "./Pages";
+import { Home } from "./pages";
 import { Auth } from "./auth";
 import "./app.css";
 import { authActions } from "./_action";
