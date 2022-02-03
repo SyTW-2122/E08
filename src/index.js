95438a222a5607f76943be088f01e3faf30961eb
alu0101166247 <alu0101166247@ull.edu.es>
2022-02-03 00:09:20 +0000

    Add test and workflows

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+// Crear el servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+// Habilitando cors
+app.use(cors());
+
+// Habilitar express.json
+app.use( express.json({ extended: true }));
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth')); 
+
+// Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando por el puerto ${PORT}`);
+})
+
+module.exports = app
