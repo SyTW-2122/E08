e30ad43643576a3305242ba8f5501aefe4082718
alu0101166247 <alu0101166247@ull.edu.es>
2022-02-08 13:01:47 +0000

    Reestructurando la base del frontend

--- src/app.js
+++ src/app.js
@@ -1,31 +1,17 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Router } from "react-router-dom";
-import { Route } from "react-router-dom";
-import history from "./_helpers/history";
-import { PrivateRoute } from "./components";
-import Navbar from "./components/NavBar";
-import Footer from "./components/Footer";
-import { Home } from "./pages";
-import { Auth } from "./auth";
-import "./app.css";
-import { authActions } from "./_action";
 
-export function App() {
-  const dispatch = useDispatch();
-  const auth = useSelector((state) => state.authReducer.auth);
 
-  const user = localStorage.getItem("user");
-  if (user) {
-    dispatch(authActions.confirmLogin(user));
-  }
+function App() {
 
   return (
-    <Router history={history}>
-      <Navbar />
-      <Route path="/auth" component={Auth} />
-      <PrivateRoute exat path="/" component={Home} auth={auth} />
-      <Footer />
-    </Router>
+    <div></div>
   );
 }
+
+export default App;
+
+
+// links de estar registrado
+// - al comprar la bateria
+// - menu usuario
+// - agregar bateria
+
